a27cb84c97b04b297cbf4a7159dfd80bdcc1e9c5
Martin Vysny <mavi@vaadin.com>
2021-12-09 12:31:52 +0200

    Add test for admin not able to navigate

---
+++ src/test/java/com/example/application/views/about/AboutViewTest.java
@@ -0,0 +1,34 @@
+package com.example.application.views.about;
+
+import com.example.application.AbstractAppTest;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.router.NotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static com.github.mvysny.kaributesting.v10.LocatorJ._assertOne;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AboutViewTest extends AbstractAppTest {
+    @BeforeEach
+    public void login() {
+        login("admin", "admin", Arrays.asList("admin"));
+    }
+
+    @Test
+    public void smokeTest() {
+        UI.getCurrent().navigate(AboutView.class);
+        _assertOne(AboutView.class);
+    }
+
+    @Test
+    public void userCantSeeThis() {
+        login("user", "user", Arrays.asList("user"));
+        final NotFoundException ex = assertThrows(NotFoundException.class, () -> {
+            UI.getCurrent().navigate(AboutView.class);
+        });
+        assertTrue(ex.getMessage().contains("No route found for 'about': Access denied"), ex.getMessage());
+    }
+}
