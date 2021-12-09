ab27184efd3a198e763d7021cb2ef407fb5e719c
Martin Vysny <mavi@vaadin.com>
2021-12-09 12:26:06 +0200

    fixed tests

--- src/test/java/com/example/application/AbstractAppTest.java
+++ src/test/java/com/example/application/AbstractAppTest.java
@@ -1,9 +1,11 @@
 package com.example.application;
 
+import com.github.mvysny.kaributesting.mockhttp.MockRequest;
 import com.github.mvysny.kaributesting.v10.MockVaadin;
 import com.github.mvysny.kaributesting.v10.Routes;
 import com.github.mvysny.kaributesting.v10.spring.MockSpringServlet;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.server.VaadinServletRequest;
 import com.vaadin.flow.spring.SpringServlet;
 import kotlin.jvm.functions.Function0;
 import org.junit.jupiter.api.AfterEach;
@@ -13,10 +15,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -41,7 +40,7 @@ public abstract class AbstractAppTest {
     @Autowired
     protected ApplicationContext ctx;
 
-    protected void login(String user, String pass, List<String> roles) {
+    protected void login(String user, String pass, final List<String> roles) {
         // taken from https://www.baeldung.com/manually-set-user-authentication-spring-security
         // also see https://github.com/mvysny/karibu-testing/issues/47 for more details.
         final List<SimpleGrantedAuthority> authorities =
@@ -50,10 +49,21 @@ public abstract class AbstractAppTest {
                 = new UsernamePasswordAuthenticationToken(user, pass, authorities);
         SecurityContext sc = SecurityContextHolder.getContext();
         sc.setAuthentication(authReq);
+
+        // however, you also need to make sure that ViewAccessChecker works properly;
+        // that requires a correct MockRequest.userPrincipal and MockRequest.isUserInRole()
+        final MockRequest request = (MockRequest) VaadinServletRequest.getCurrent().getRequest();
+        request.setUserPrincipalInt(authReq);
+        request.setUserInRole((principal, role) -> roles.contains(role));
     }
 
     protected void logout() {
         SecurityContextHolder.getContext().setAuthentication(null);
+        if (VaadinServletRequest.getCurrent() != null) {
+            final MockRequest request = (MockRequest) VaadinServletRequest.getCurrent().getRequest();
+            request.setUserPrincipalInt(null);
+            request.setUserInRole((principal, role) -> false);
+        }
     }
 
     @BeforeEach
