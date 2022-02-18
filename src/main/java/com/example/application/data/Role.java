de973c9c5b711edbf37fe40bcef0dca21f80c107
Martin Vysny <mavi@vaadin.com>
2022-02-18 10:48:50 +0200

    add jetbrains annotations

--- src/main/java/com/example/application/data/Role.java
+++ src/main/java/com/example/application/data/Role.java
@@ -1,16 +1,19 @@
 package com.example.application.data;
 
+import org.jetbrains.annotations.NotNull;
+
 public enum Role {
     USER("user"), ADMIN("admin");
 
+    @NotNull
     private final String roleName;
 
-    Role(String roleName) {
+    Role(@NotNull String roleName) {
         this.roleName = roleName;
     }
 
+    @NotNull
     public String getRoleName() {
         return roleName;
     }
-
 }
